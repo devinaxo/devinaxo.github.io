ea915c7f596d8d28e0e31242a219d53d85078e06
devinaxo <brian456251@gmail.com>
2024-04-27 23:58:30 -0300

    Added "My Info" window, minor changes

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var d;
+var d, pw = 'win3', pb = 'portfolio-btn';
 
 function updateTime(){
     d = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
@@ -18,4 +18,16 @@ function closeFolder(closeButton){
     let folderId = winId.replace('win', 'icon')
     document.getElementById(winId).style.display = "none";
     document.getElementById(folderId).src = 'img/folder.png'
+}
+function manipulateWindow(code){
+    switch(code){
+        case 1:
+            document.getElementById(pw).style.display = 'none';
+            document.getElementById(pb).style.display = 'block';
+            break;
+        case 2:
+            document.getElementById(pw).style.display = 'block';
+            document.getElementById(pb).style.display = 'none';
+            break;
+    }
 }
