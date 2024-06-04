031a347462ec4f2c71d19875a98e4b1b84beaac4
devinaxo <brian456251@gmail.com>
2024-06-03 23:39:58 -0300

    Translated all js into jquery Finally, unobtrusive javascript

--- js/app.js
+++ js/app.js
@@ -2,25 +2,109 @@ function updateTime(){
     let d = new Date().toTimeString().replace(/.*(\d{2}:\d{2}:\d{2}).*/, "$1");
     document.getElementById('time').innerText = d;
 }
+var players = {};
+function onYouTubeIframeAPIReady() {
+    $('iframe').each(function() {
+        var iframeId = $(this).attr('id');
+        players[iframeId] = new YT.Player(iframeId);
+    });
+}
+
 $(document).ready(function(){
     updateTime();
     setInterval(updateTime, 1000);
+
     $( ".window" ).draggable({ handle: ".title-bar" });
+
+    const notme = $('#notme');
     $('#cc-btn').on('click', function(){
-        if($('#notme').attr('type') == 'email'){
-            $('#notme').attr('type', 'text');
-            $('#notme').attr('placeholder', 'Name (Click button to change Cc signature)');
+        if(notme.attr('type') == 'email'){
+            notme.attr('type', 'text');
+            notme.attr('placeholder', 'Name (Click button to change Cc signature)');
         }else{
-            $('#notme').attr('type', 'email');
-            $('#notme').attr('placeholder', 'E-mail (Click button to change Cc signature)');
+            notme.attr('type', 'email');
+            notme.attr('placeholder', 'E-mail (Click button to change Cc signature)');
         }
-        $('#notme').val('');
+        notme.val('');
     });
-    $('#notme').keypress(function(e){
-        if($('#notme').attr('type') == 'text'){
+
+    notme.keypress(function(e){
+        if(notme.attr('type') == 'text'){
             if(String.fromCharCode(e.keyCode).match(/[^a-zA-Z áéíóú ÁÉÍÓÚ]/gi)) return false;
         }
     });
+
+    const clickableFolders = $('.clickable-folder');
+    let currWin;
+    let currIcon;
+    clickableFolders.on('click', function(){
+        $(this).css('background-color', 'gray');
+        clickableFolders.not(this).each(function(){
+            $(this).css('background-color', 'transparent');
+        })
+    })
+
+    clickableFolders.on('dblclick', function(){
+        $('.needs-closing').hide();
+        $('.image-folder').each(function(){
+            $(this).attr('src', 'img/directory_closed_cool-0.png');
+        })
+        $(this).css('background-color', 'transparent');
+        currWin = $(this).data('window');
+        $('#' + currWin).show();
+        currIcon = $(this).data('icon');
+        $('#' + currIcon).attr('src', 'img/directory_open_cool-0.png');
+    })
+
+    $('.window-close').on('click', function(){
+        currWin = $(this).data('window');
+        $('#' + currWin).hide();
+        currIcon = $(this).data('icon');
+        $('#' + currIcon).attr('src', 'img/directory_closed_cool-0.png');
+    })
+
+    $('.window-minimize').on('click', function(){
+        currWin = $(this).data('window');
+        $('#' + currWin).hide();
+        currIcon = $(this).data('icon');
+        $('#' + currIcon).attr('src', 'img/portfolio-btn.png');
+    })
+    $('#portfolio-btn').on('click', function(){
+        currWin = $(this).data('window');
+        $('#' + currWin).show();
+        currIcon = $(this).data('icon');
+        $('#' + currIcon).attr('src', 'img/portfolio-btn-pressed.png');
+    })
+
+    $(document).ready(function() {
+        $('[data-trigger-modal]').each(function() {
+            var $iconSpot = $(this);
+            var $image = $iconSpot.find('img');
+            var modalId = $image.attr('id').replace('image', 'modal');
+            var $modal = $('#' + modalId);
+            var $modalImg = $modal.find('.modal-content');
+            var hqImage = $image.attr('alt');
+            var closeId = $image.attr('id').replace('image', 'close');
+            var $closeBtn = $('#' + closeId);
+            var $iframeContainer = $modal.find('.iframe-container');
+            var iframeHtml = $iframeContainer.html(); // Save the original iframe HTML
+    
+            $iconSpot.on('dblclick', function() {
+                $modal.show();
+                $modalImg.attr('src', hqImage);
+            });
+            $closeBtn.on('click', function() {
+                $modal.hide();
+                if ($iframeContainer.length) {
+                    $iframeContainer.html(iframeHtml);
+                }
+            });
+            $iconSpot.on('click', function() {
+                $iconSpot.css('background-color', 'unset');
+            });
+        });
+    });
+
     const btn = document.getElementById('button');
     document.getElementById('contact-form').addEventListener('submit', function(event) {
         event.preventDefault();
@@ -39,67 +123,3 @@ $(document).ready(function(){
         });
     });
 });
-
-function highlight(div){
-    div.style.backgroundColor = 'gray';
-}
-
-function openFolder(folder){
-    folder.parentNode.style.backgroundColor = 'unset';
-    let winId = folder.id.replace('icon', 'win');
-    document.getElementById(winId).style.display = "block";
-    folder.src = 'img/directory_open_cool-0.png';
-    if(winId.endsWith('1')){
-        winId = winId.replace('1', '2');
-        document.getElementById(winId).style.display = "none";
-        document.getElementById(winId.replace('win', 'icon')).src = 'img/directory_closed_cool-0.png';
-    }else{
-        winId = winId.replace('2', '1');
-        document.getElementById(winId).style.display = "none";
-        document.getElementById(winId.replace('win', 'icon')).src = 'img/directory_closed_cool-0.png';
-    }
-}
-
-function closeFolder(closeButton){
-    let winId = closeButton.parentNode.parentNode.parentNode.id;
-    let folderId = winId.replace('win', 'icon');
-    document.getElementById(winId).style.display = "none";
-    document.getElementById(folderId).src = 'img/directory_closed_cool-0.png';
-}
-
-function manipulateWindow(opt){
-    switch(opt){
-        case 1: //close
-            document.getElementById('win3').style.display = 'none';
-            document.getElementById('portfolio-btn').src = 'img/portfolio-btn.png';
-            break;
-        case 2: //open
-            document.getElementById('win3').style.display = 'block';
-            document.getElementById('portfolio-btn').src = 'img/portfolio-btn-pressed.png';
-            break;
-    }
-}
-
-function triggerModal(image){
-    //unset gray color
-    image.parentNode.style.backgroundColor = 'unset';
-    // Get the modal
-    var modal = document.getElementById(image.id.replace("image", "modal"));
-
-    // Get the image and insert it inside the modal
-    var img = document.getElementById(image.id);
-    var modalImg = document.getElementById(image.id.replace("image", "img"));
-    var hqImage = image.alt;
-    img.ondblclick = function(){
-        modal.style.display = "block";
-        modalImg.src = hqImage;
-    }
-
-    // Get the <span> element that closes the modal
-    var span = document.getElementById(image.id.replace("image", "close"));
-
-    // When the user clicks on <span> (x), close the modal
-    span.onclick = function() { 
-    modal.style.display = "none";
-    }
-}
